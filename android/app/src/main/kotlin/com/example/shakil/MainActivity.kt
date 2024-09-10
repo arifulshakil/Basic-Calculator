d0ed88396b016610a3e5fa715455c6f3468672b5
arifu <arifulislamshakil6@gmail.com>
2024-09-10 09:53:39 +0600

    login & register

---
+++ android/app/src/main/kotlin/com/example/shakil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shakil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'registation.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
